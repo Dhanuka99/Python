0948ab6f57219775426d5849e6dc00baa99c76f2
Dhanuka Lakshan <danuka1999lakshan@gmail.com>
2022-08-14 13:49:07 +0530

    InputOutput

---
+++ InputOutput.py
@@ -0,0 +1,11 @@
+text = input("Enter Text : ")
+print("User Text is ",text) #User Text is  hello folks
+
+username = "Dhanuka"
+password =1234
+
+print("Hello! Welcome to the github. Client user name is {}. and password is {}".format(username,password))
+#Hello! Welcome to the github. Client user name is Dhanuka. and password is 1234
+
+print("Hello! Welcome to the github. Client user name is {1}. and password is {0}".format(username,password))
+#Hello! Welcome to the github. Client user name is 1234. and password is Dhanuka
