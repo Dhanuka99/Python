daaaae45942d108af3b0bb7fd8028319d5282025
Dhanuka Lakshan <danuka1999lakshan@gmail.com>
2022-08-24 22:55:15 +0530

    data types

---
+++ Variable_DataTypes.py
@@ -0,0 +1,28 @@
+a = 1
+b = 1.15
+c = True
+d = "Dhanuka"
+e = 1j
+f = ["apple","banana","cherry"]
+g = ("apple","banana","cherry")
+h = range(6)
+i = {"name":"dhanuka","password":"1234"}
+j = frozenset({"apple", "banana", "cherry"})
+k = b"Hello"
+l = bytearray(5)
+m = memoryview(bytes(5))
+n = None
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+print(type(h))
+print(type(i))
+print(type(j))
+print(type(k))
+print(type(l))
+print(type(m))
+print(type(n))
